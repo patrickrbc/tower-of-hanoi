18776863dbf24735300d8cdf4cc2f492b7f75d16
Patrick Costa <patrickrbcosta@gmail.com>
2013-07-22 01:24:29 -0300

    changes

--- hanoi.s
+++ hanoi.s
@@ -1,58 +1,95 @@
 section .data
-        hello: db 'Hi, this is a Tower of Hanoi solution! Tell me how many disks you have.', 10
-        hellolen: equ $-hello
-        step: db 'move disk N from A to B', 10
-        steplen: equ $-step
-        noteq: db 'not equal', 10
-        eq: db 'equal', 10
+	hello: db 'Hi, this is a Tower of Hanoi solution! Tell me how many disks you have.', 10
+	hellolen: equ $-hello
+
+	mova: db 'move disk', 32
+	de: db 32, 'from', 32
+	para: db 32, 'to', 32
+
+	diskA: db 65
+	diskB: db 66
+	diskC: db 67
 
 section .text
-        global _start
+    global _start
 
-_hanoi:
-;	print input
+
+_print:
+;	print 'move disk'
 	mov eax, 4
 	mov ebx, 0
-	mov ecx, step
-	mov edx, steplen
-	int 80h
-
-	mov ecx, 3
-	mov edx, 4
-	cmp ecx, edx
-
-	je equal
-	mov eax, 4
-	mov ebx, 0
-	mov ecx, noteq
+	mov ecx, mova
 	mov edx, 10
 	int 80h
-	jmp ae
 
-	equal:
+;	print disk number
 	mov eax, 4
 	mov ebx, 0
-	mov ecx, eq
+	mov ecx, nDisk
+	mov edx, 10
+	int 80h
+
+;	print 'from'
+	mov eax, 4
+	mov ebx, 0
+	mov ecx, de
 	mov edx, 6
 	int 80h
 
-	ae:
+	mov eax, 4
+	mov ebx, 0
+	mov ecx, diskA
+	mov edx, 1
+	int 80h
+
+;	print 'to'
+	mov eax, 4
+	mov ebx, 0
+	mov ecx, para
+	mov edx, 4
+	int 80h
+
+	mov eax, 4
+	mov ebx, 0
+	mov ecx, diskB
+	mov edx, 1
+	int 80h
+
+	ret
+
+
+_hanoi:
+;	compare with one
+	mov eax, dword[nDisk]
+	cmp eax, 49
+
+	je equal
+;	not equal
+;	call hanoi n-1, a, c, b
+;	call _print
+;	call hanoi n-1, c, b, a
+	jmp done
+
+	equal:
+	call _print
+
+	done:
 	ret
 
 _start:
 ;	welcome message
-    mov eax, 4
-    mov ebx, 0
-    mov ecx, hello
-    mov edx, hellolen
-    int 80h
+	mov eax, 4
+	mov ebx, 0
+	mov ecx, hello
+	mov edx, hellolen
+	int 80h
 
 ;	read input
-;	mov eax, 3
-;	mov ebx, 0
-;	mov ecx, input
-;	mov edx, 1
-;	int 80h
+	mov eax, 3
+	mov ebx, 0
+	mov ecx, nDisk
+	mov edx, 1
+	int 80h
 
 	call _hanoi
 
@@ -61,6 +98,5 @@ _start:
     mov ebx, 0
     int 80h
 
-
 section .bss
-	input resb 256
+	nDisk resb 4
